f273f40cc3ac5985de3f732781d3bc0c29b2898a
sszahinos <sergiosaza6@gmail.com>
2023-03-15 09:37:26 +0100

    ex01

---
+++ Module 02/ex01/main.py
@@ -0,0 +1,41 @@
+def what_are_the_vars(*args, **kwargs):
+    obj = ObjectC()
+    for key, value in kwargs.items():
+        setattr(obj, key, value)
+    for index, arg in enumerate(args):
+        setattr(obj, "var_{}".format(index), arg)
+    return obj
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+
+#Defined by the subject#
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+if __name__ == "__main__":
+    #what_are_the_vars("asdf", test="test_value" )
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars(None, [])
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, "Yes", a=10, var_2="world")
+    doom_printer(obj)
